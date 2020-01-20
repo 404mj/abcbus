099ddc87c7218ab8f8a3cbbdd19d5099c56c6cbb
404mj <zhangshuxinmj@163.com>
2020-01-20 18:44:47 +0800

    add model begin

--- collect/models.py
+++ collect/models.py
@@ -1,2 +1,14 @@
 from django.db import models
 
+class PersonalFinance(models.Model):
+    pfid=models.AutoField(primary_key=True)
+    memo=models.CharField()
+    subbrch_net=models.IntegerField()
+    bank_subbrch=models.IntegerField()
+    submitter=models.CharField(max_length=50,default='',)
+    submit_time=models.DateTimeField(auto_now_add=True)
+
+    large_deposit=models.CharField()
+    precious_metal=models.CharField()
+    found=models.CharField()
+
